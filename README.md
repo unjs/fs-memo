fe28a40b70ef51276ced382ed4bcea7368f591bf
pooya parsa <pooya@pi0.ir>
2020-05-31 22:43:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# fs-memo
+
+> Easy persisted memo object for Node.js
+
+[![npm](https://img.shields.io/npm/dt/fs-memo.svg?style=flat-square)](https://npmjs.com/package/fs-memo)
+[![npm (scoped with tag)](https://img.shields.io/npm/v/fs-memo/latest.svg?style=flat-square)](https://npmjs.com/package/fs-memo)
+
+## Usage
+
+Install package:
+
+```bash
+yarn add fs-memo
+# or
+or npm install fs-memo
+```
+
+```js
+const { getMemo, setMemo } = require('fs-memo')
+// or
+import { getMemo, setMemo } from 'fs-memo'
+```
+
+
+### `getMemo(options)`
+
+```ts
+getMemo(options: MemoOptions): Promise<object>
+```
+
+Load latest memo from file-system and combine with local state from CJS cache.
+
+FS loading silently bails if:
+ - The process that made memo is still alive with different pid
+ - Any fs error happens (like permission denied)
+
+### `setMemo(eoptions)`
+
+```ts
+setMemo(memo: object, options: MemoOptions): Promise<void>
+```
+
+Update local state from CJS cache and persist memo object to file-system.
+
+FS persistence silently bails if any error happens.
+
+## Options
+
+```ts
+interface MemoOptions {
+  dir?: string
+  name?: string
+}
+```
+
+### `dir`
+
+Specify directory where memo file should be stored. Default dir is `__dirname` (`node_modules/fs-memo/dist`)
+
+### `name`
+
+Name of memo file. Default name is `.fs-memo`.
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { promises as fs } from 'fs'
+import { resolve } from 'path'
+import { pid } from 'process'
+
+const _memo = {
+  _pid: process.pid
+}
+
+const defaults = {
+  dir: __dirname,
+  name: '.getport'
+}
+
+type MemoOptions = Partial<typeof defaults>
+
+export async function getMemo (options: MemoOptions): Promise<object> {
+  const { file } = getOptions(options)
+
+  // Try to load latest memo
+  try {
+    const memo = JSON.parse(await fs.readFile(file, 'utf-8'))
+    if (
+      !isAlive(pid) || // RIP
+      memo._pid === _memo._pid // fs is more reliable than require cache
+    ) {
+      Object.assign(_memo, memo)
+      _memo._pid = process.pid
+    }
+  } catch (e) {
+    // Ignore
+  }
+
+  return _memo
+}
+
+export async function setMemo (memo: object, options: MemoOptions): Promise<void> {
+  const { file } = getOptions(options)
+
+  // Set local memo
+  Object.assign(_memo, memo)
+  _memo._pid = process.pid
+
+  // Try to persist
+  try {
+    await fs.writeFile(file, JSON.stringify(_memo), 'utf-8')
+  } catch (e) {
+    // Ignore
+  }
+}
+
+function getOptions (options: MemoOptions): typeof defaults & { file: string } {
+  const opts: any = { ...defaults, options }
+  opts.file = resolve(opts.dir, opts.name)
+  return opts
+}
+
+function isAlive (pid: number): Boolean {
+  try {
+    process.kill(pid, 0)
+    return true
+  } catch (e) {
+    return e.code === 'EPERM'
+  }
+}
